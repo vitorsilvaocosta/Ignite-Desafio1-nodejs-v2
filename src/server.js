94e1fefefab4cce9f90a2a0dda9b1c007644045a
Vitor Silva <vitorotiv_silva@hotmail.com>
2021-04-22 06:56:23 -0300

    commit swagger

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 const app = require('./');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger.json');
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.listen(3333);
